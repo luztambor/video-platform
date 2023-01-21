487aa6a49cac9c61f8f8223f04c44bb483ba20c3
jwms <willia32@stolaf.edu>
2023-01-21 17:16:23 -0500

    create empty components, compile without errors

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Box } from "@mui/material";
+import Navbar from "./components/Navbar";
+import Feed from "@mui/icons-material";
+import VideoDetail from "./components/VideoDetail";
+import ChannelDetail from "./components/ChannelDetail";
+import SearchFeed from "./components/SearchFeed";
 
 const App = () => (
   <BrowserRouter>
     <Box>
-      {" "}
       sx={{ backgroundColor: "#000" }}
       <Navbar />
       <Routes>
