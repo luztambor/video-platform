b954b061a79133ec9d5edf9c2893d400f678070f
jwms <willia32@stolaf.edu>
2023-01-23 15:34:56 -0500

    point before mapping trouble for card display

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -1,8 +1,8 @@
-import React from "react";
+import { Stack, Box, Typography } from "@mui/material";
+import { VideoCard, ChannelCard } from "./";
 
 const Videos = ({ videos }) => {
   console.log(videos);
-
   return <div>Videos</div>;
 };
 
