62f58525c4794ee9f7891b7a35092671ee9db2ed
jwms <willia32@stolaf.edu>
2023-01-24 13:44:39 -0500

    tweak card layout

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -2,7 +2,6 @@ import { Link } from "react-router-dom";
 import { Typography, Card, CardContent, CardMedia } from "@mui/material";
 import { CheckCircle } from "@mui/icons-material";
 import {
-  demoThumbnailUrl,
   demoVideoUrl,
   demoVideoTitle,
   demoChannelTitle,
@@ -18,7 +17,7 @@ const VideoCard = ({
   return (
     <Card
       sx={{
-        maxWidth: "350px",
+        width: { xs: "100%", sm: "358px", md: "320px" },
         boxShadow: "none",
         borderRadius: 0,
         justifyContent: "center",
@@ -29,7 +28,11 @@ const VideoCard = ({
         <CardMedia
           image={snippet?.thumbnails?.high?.url}
           alt={snippet?.title}
-          sx={{ width: 358, height: 180, justifyContent: "center" }}
+          sx={{
+            width: { xs: "100%", sm: "358", md: "320px" },
+            height: 180,
+            justifyContent: "center",
+          }}
         />
       </Link>
       <CardContent sx={{ backgroundColor: "#1e1e1e", height: "106px" }}>
