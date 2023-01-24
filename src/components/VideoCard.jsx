14ebd73d282f0d2e1b8be36bc89f718d41f6b2aa
jwms <willia32@stolaf.edu>
2023-01-24 12:53:04 -0500

    add video player, recommended video population

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -18,16 +18,18 @@ const VideoCard = ({
   return (
     <Card
       sx={{
-        width: { md: "320px", xs: "100%" },
+        maxWidth: "350px",
         boxShadow: "none",
         borderRadius: 0,
+        justifyContent: "center",
+        alignItems: "center",
       }}
     >
       <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
         <CardMedia
           image={snippet?.thumbnails?.high?.url}
           alt={snippet?.title}
-          sx={{ width: 358, height: 180 }}
+          sx={{ width: 358, height: 180, justifyContent: "center" }}
         />
       </Link>
       <CardContent sx={{ backgroundColor: "#1e1e1e", height: "106px" }}>
