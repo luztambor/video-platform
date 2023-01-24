4af83e7cff9a50deab1cf79c149cc744690b6fb9
jwms <willia32@stolaf.edu>
2023-01-23 20:01:30 -0500

    propagate videoCard info

--- src/components/ChannelCard.jsx
+++ src/components/ChannelCard.jsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 const ChannelCard = () => {
   return <div>ChannelCard</div>;
 };
--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -3,9 +3,12 @@ import { VideoCard, ChannelCard } from "./";
 
 const Videos = ({ videos }) => {
   return (
-    <Stack>
+    <Stack direction="row" flexWrap="wrap" justifyContent="start" gap={2}>
       {videos.videos.map((item, idx) => (
-        <Box key={idx}> {item.id.videoId} </Box>
+        <Box key={idx}>
+          {item.id.videoId && <VideoCard video={item} />}
+          {item.id.channelId && <ChannelCard channelDetail={item} />}
+        </Box>
       ))}
     </Stack>
   );
